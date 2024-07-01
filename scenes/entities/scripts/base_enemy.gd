b5e7a1c1535f033ac292ef2c6f09e3dfd00c4f7d
HyxerX <kamil2005kr@gmail.com>
2024-07-01 15:13:02 +0200

    pierwsze kroki

---
+++ scenes/entities/scripts/base_enemy.gd
@@ -0,0 +1,20 @@
+extends PathFollow2D
+
+
+@export_category("properties")
+@export var health : float = 10.0
+@export var speed : float = 4.0
+
+signal EndOfPath(damage)
+
+func _process(delta):
+	progress += speed * delta
+	if progress_ratio == 1:
+		dealDamage(health)
+		emit_signal("EndOfPath", health)
+		print(EndOfPath)
+		queue_free()
+
+func dealDamage(health):
+	# TODO
+	pass
